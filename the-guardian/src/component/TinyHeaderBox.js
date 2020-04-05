69663d9260fa0afda8fc1e2b7ca8ab66d9094bad
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-05 18:13:35 -0500

    added wireframe, and header and tinyhader component

---
+++ the-guardian/src/component/TinyHeaderBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TinyHeaderBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default TinyHeaderBox;
