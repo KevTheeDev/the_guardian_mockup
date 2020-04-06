cc1c439df5d08c709f62b288adaf23a4a1b6c47d
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-05 20:06:09 -0500

    added header and tinyHeaderBox components to app.js, and also axios isn't working, going to repl.it

--- the-guardian/src/App.js
+++ the-guardian/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Header from './component/Header';
+import TinyHeaderBox from './component/TinyHeaderBox';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <TinyHeaderBox />
     </div>
   );
 }
-
-export default App;
